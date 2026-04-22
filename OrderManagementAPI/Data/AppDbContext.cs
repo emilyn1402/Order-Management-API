ef1e9942c30fa04c6fb4173a4539c96c91d95d9b
Emilyn Pascua <emilyn1402@gmail.com>
2026-04-23 00:02:44 +0800

    Add DbContext and create initial SQL Server schema

---
+++ OrderManagementAPI/Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderManagementAPI.Models;
+
+namespace OrderManagementAPI.Data
+{
+    //Manage database context
+    public class AppDbContext : DbContext
+    {
+        //Constructor receives DB options from Program.cs
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+        }
+
+        //These become tables
+        public DbSet<ProductModel> Products { get; set; }
+        public DbSet<OrderModel> Orders { get; set; }
+        public DbSet<OrderItemModel> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Configure decimal precision for money values
+            modelBuilder.Entity<ProductModel>()
+                .Property(p => p.Price)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<OrderModel>()
+                .Property(p => p.TotalAmount)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<OrderItemModel>()
+                .Property(p => p.Price)
+                .HasPrecision(18, 2);
+        }
+    }
+}
--- OrderManagementAPI/Program.cs
+++ OrderManagementAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using OrderManagementAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register database connection
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer("Server=localhost;Database=OrderManagementDB;Trusted_Connection=True;TrustServerCertificate=True;"));
 
 var app = builder.Build();
 
